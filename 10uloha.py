4610b40245a84a92feff6f445e31d08a012c694e
SoskinYoung <163108396+SoskinYoung@users.noreply.github.com>
2024-03-19 15:28:39 +0100

    10uloha.py

--- 10uloha.py
+++ 10uloha.py
@@ -1,11 +1,13 @@
-def vypis_interval(a, b):
-    for i in range(a, b + 1):
-        print(i, end="")
-        if i < b:
-            print(", ", end="")
-    print()
-a = int(input("Zadejte počáteční hodnotu intervalu: "))
-b = int(input("Zadejte koncovou hodnotu intervalu: "))
-
-print("Čísla v intervalu <" + str(a) + ";" + str(b) + ">: ", end="")
-vypis_interval(a, b)
+def je_prvocislo(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+def vypis_prvocisla(do_cisla):
+    print("Prvočísla do", do_cisla, "jsou:")
+    for number in range(2, do_cisla + 1):
+        if je_prvocislo(number):
+            print(number, end=" ")
+n = int(input("Zadejte číslo n: "))
