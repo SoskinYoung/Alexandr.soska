47f9ec3c44d0119d81c22a8db115160bfcfd221f
SoskinYoung <163108396+SoskinYoung@users.noreply.github.com>
2024-03-19 16:56:48 +0100

    Update 20uloha.py

--- 20uloha.py
+++ 20uloha.py
@@ -3,3 +3,9 @@ pole = [random.randint(1, 100) for _ in range(10)]
 print("Náhodná čísla v poli:")
 for prvek in pole:
     print(prvek)
+nejmensi = min(pole)
+nejvetsi = max(pole)
+prumer = sum(pole) / len(pole)
+print("\nNejmenší hodnota v poli je:", nejmensi)
+print("Největší hodnota v poli je:", nejvetsi)
+print("Průměrná hodnota v poli je:", prumer)
